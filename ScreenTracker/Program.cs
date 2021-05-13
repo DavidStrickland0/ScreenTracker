cfc794061ca1ba94c1dee08dbdacd02a93a2ad3f
David Strickland <david.Strickland@email.address>
2021-05-13 17:42:53 -0500

    Initial Commit

---
+++ ScreenTracker/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ScreenTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    Assembly.GetExecutingAssembly().GetName().Name);
+                Directory.CreateDirectory(path);
+                while (true)
+                {
+
+                    var nameStamp = DateTime.Now.ToString("yyyyMMddhhmmss");
+                    Console.WriteLine($"Capturing {nameStamp}");
+                    var fileName = System.IO.Path.Combine(path, nameStamp + ".jpg");
+                    ScreenCapture.CaptureScreenToFile(fileName, ImageFormat.Jpeg);
+                    Task.Delay(new TimeSpan(0, 5, 0)).Wait();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
---
+++ ScreenTracker/ScreenCapture.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+
+namespace ScreenTracker
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
+        /// <summary>
+        /// Provides functions to capture the entire screen, or a particular window, and save it to a file.
+        /// </summary>
+        public class ScreenCapture
+        {
+
+            /// <summary>
+            /// Captures a screen shot of the entire desktop, and saves it to a file
+            /// </summary>
+            /// <param name="filename"></param>
+            /// <param name="format"></param>
+            public static void CaptureScreenToFile(string filename, ImageFormat format)
+            {
+                using var bitmap = new Bitmap(SystemInformation.VirtualScreen.Right, SystemInformation.VirtualScreen.Bottom);
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0,
+                        bitmap.Size, CopyPixelOperation.SourceCopy);
+                }
+                bitmap.Save(filename,ImageFormat.Jpeg);
+            }
+
+        }
+    
+}
